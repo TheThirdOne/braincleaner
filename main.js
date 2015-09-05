239ccdb68e8a183d5c4c3d58e9d1dee529926182
Benjamin Landers <benjaminrlanders@gmail.com>
2015-09-05 14:26:50 -0700

    fixed some style

--- main.js
+++ main.js
@@ -6,27 +6,27 @@ var stack = 0;
 var createMacro = function(name,stackDiff,str){
   fcns[name] = function(){
     emit(str,stackDiff);
-  }
-}
+  };
+};
 
-createMacro("dup",     1, "[->+>+<<]>>[-<<+>>]<")
-createMacro("dupnot",  1, "-[+>+>+<<]>>-[<<+>>+]<")
-createMacro("not",     0, "-[+>+<]>[-<+>]<")
-createMacro("xor",    -1, "<[->+<]+>-[<->+[-]]<")
-createMacro("xnor",   -1, "<[->+<]>-[<+>+[-]]<")
-createMacro("and",    -1, "<[->+<]+>--[<->[+]]<")
-createMacro("add",    -1, "[-<+>]<")
-createMacro("mul",    -1, "<[->[->+>+<<]>>[-<<+>>]<<<]>[-]>[-<<+>>]<<")
-createMacro("eq",     -1, "<[->-<]+>[<->[-]]<")
-createMacro("neq",    -1, "<[->-<]>[<+>[-]]<")
-createMacro("nand",   -1, "<[->+<]>--[<+>[+]]<")
-createMacro("or",     -1, "<[->+<]>[<+>[-]]<")
-createMacro("nor",    -1, "<[->+<]+>[<->[-]]<")
-createMacro("swap",    0, "<[->>+<<]>[-<+>]>[-<+>]<")
-createMacro("pop",    -1, "[-]<")
-createMacro("scans",   1, ">,")
-createMacro("scand",   1, ">,>++++[-<------------>]<")
-createMacro("printd",  1, ">++++[-<++++++++++++>]<.[-]<")
+createMacro("dup",     1, "[->+>+<<]>>[-<<+>>]<");
+createMacro("dupnot",  1, "-[+>+>+<<]>>-[<<+>>+]<");
+createMacro("not",     0, "-[+>+<]>[-<+>]<");
+createMacro("xor",    -1, "<[->+<]+>-[<->+[-]]<");
+createMacro("xnor",   -1, "<[->+<]>-[<+>+[-]]<");
+createMacro("and",    -1, "<[->+<]+>--[<->[+]]<");
+createMacro("add",    -1, "[-<+>]<");
+createMacro("mul",    -1, "<[->[->+>+<<]>>[-<<+>>]<<<]>[-]>[-<<+>>]<<");
+createMacro("eq",     -1, "<[->-<]+>[<->[-]]<");
+createMacro("neq",    -1, "<[->-<]>[<+>[-]]<");
+createMacro("nand",   -1, "<[->+<]>--[<+>[+]]<");
+createMacro("or",     -1, "<[->+<]>[<+>[-]]<");
+createMacro("nor",    -1, "<[->+<]+>[<->[-]]<");
+createMacro("swap",    0, "<[->>+<<]>[-<+>]>[-<+>]<");
+createMacro("pop",    -1, "[-]<");
+createMacro("scans",   1, ">,");
+createMacro("scand",   1, ">,>++++[-<------------>]<");
+createMacro("printd", -1, ">++++[-<++++++++++++>]<.[-]<");
 
 fcns.prints = function(s){
   var code = ">";
@@ -43,15 +43,17 @@ fcns.prints = function(s){
   }
   code += "[-]<";
   emit(code,0);
-}
+};
+
 fcns.loadc = function(c){
   var out = ">";
   for(var i = 0; i < c;i++){
     out+="+";
   }
   emit(out,1);
-}
-fcns.loadv = function(label){
+};
+
+fcns.loadv = function(label,dup){
   var l="",r="",out;
   if(!vars[label]){
     throw "Unknown variable";
@@ -65,9 +67,10 @@ fcns.loadv = function(label){
     l += "<";
     r += ">";
   }
-  out = l+"[-"+r+"+"+l+"]"+r;
-  emit(out,0);
-}
+  out = l+"[-"+r+(dup?"+>+<":"+")+l+"]"+r+(dup?">":"");
+  emit(out,dup?1:0);
+};
+
 fcns.storev = function(label){
   var l="",r="",out;
   if(!vars[label]){
@@ -83,19 +86,21 @@ fcns.storev = function(label){
   }
   out = "[-"+l+"+"+r+"]<";
   emit(out,-1);
-}
+};
+
 fcns.load = function(label){
-  fcns.loadv(label);
-  fcns.dup();
+  fcns.loadv(label,true);
   fcns.storev(label);
-}
+};
+
 fcns.createVar = function(label){
   emit(">",1);
-  fcns.createLabel(label)
-}
+  fcns.createLabel(label);
+};
+
 fcns.createLabel = function(label){
   vars[label] = stack;
-}
+};
 
 //format stack | cond
 fcns.if = function(code){
@@ -103,27 +108,29 @@ fcns.if = function(code){
   var tmp = stack;
   code();
   if(tmp !== stack){
-    throw "Code inside if causes net stack change"
+    throw "Code inside if causes net stack change";
   }
   emit("-]<",-1);
-}
+};
+
 fcns.ifelse = function(a,b){
   fcns.dup();
   emit("[",0);
   var tmp = stack;
   a();
   if(tmp !== stack){
-    throw "Code inside if causes net stack change"
+    throw "Code inside if causes net stack change";
   }
   emit("-]<",-1);
   emit("-[",0);
   var temp = stack;
   b();
   if(temp !== stack){
-    throw "Code inside if causes net stack change"
+    throw "Code inside if causes net stack change";
   }
   emit("+]<",-1);
-}
+};
+
 fcns.whilenot = function(c,code){
   var neg = "", pos = "";
   for(var i = 0; i < c; i++){
@@ -135,10 +142,10 @@ fcns.whilenot = function(c,code){
   var temp = stack;
   code();
   if(temp !== stack){
-    throw "Code inside while causes net stack change"
+    throw "Code inside while causes net stack change";
   }
   emit(neg + "]",0);
-}
+};
 
 //format: old stack | cond | run (generated)
 fcns.switch = function(cases,triggers,def){
@@ -149,7 +156,7 @@ fcns.switch = function(cases,triggers,def){
   var previous = 0;
   var code = "";
   for(var item = 0; item < triggers.length; item++){
-    var diff = previous - triggers[item]
+    var diff = previous - triggers[item];
     var sign = diff > 0?"+":"-";
     diff = diff > 0?diff:-diff;
     for(var i = 0; i < diff; i++){
@@ -160,19 +167,20 @@ fcns.switch = function(cases,triggers,def){
   }
   emit(code,0);
   emit(">-",0);   //set run = false
-  def()
+  def();
   emit("<[-]",0); //set cond = 0
   for(var i = cases.length-1; i >= 0; i--){
     emit("]>[-",0); //set run = false
-    cases[i]()
+    cases[i]();
     emit("]<",0);
   }
   emit("<",-2);
-}
+};
+
 var emit = function(str,diff){
   code += str;
   stack += diff;
   if(stack < 0){
     throw "Stack too small";
   }
-}
+};
