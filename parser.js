2bfa74e1bbfdcc9a1aaa475eabe1d5438c89e657
Benjamin Landers <benjaminrlanders@gmail.com>
2015-09-08 02:38:29 -0700

    Add redumentary parsing

--- parser.js
+++ parser.js
@@ -5,6 +5,11 @@ parser.switch = function(){
   c.type = "switch";
   context.push(c);
 };
+parser.whilenot = function(){
+  var c = [];
+  c.type = "whilenot";
+  context.push(c);
+};
 parser.end= function(){
   var last = context.pop();
   context[context.length-1].push(last);
@@ -44,25 +49,63 @@ parser.inst = function(inst){
   }
   context[context.length-1].push({type:"inst",fcn:fcns[inst]});
 };
-
+parser.prints = function(line){
+  context[context.length-1].push({type:"inst",fcn:fcns.prints,args:[line]});
+};
 parser.parse = function(code){
   var lines = code.split("\n");
   for(var i = 0; i < lines.length;i++){
     if(lines[i] === "switch"){
       parser.switch();
-      console.log("switch")
+    }else if(lines[i] === "whilenot"){
+      parser.whilenot();
     }else if(lines[i] === "break"){
       parser.break();
-      console.log("break")
     }else if(lines[i] === "end"){
       parser.end();
-      console.log("end")
     }else if(lines[i].startsWith("case")){
       parser.case(parseInt(lines[i].split(" ")[1]));
-      console.log("case")
+    }else if(lines[i].startsWith("prints")){
+      parser.prints(lines[i].slice(7));
     }else{
-      console.log(lines[i])
       parser.inst(lines[i])
     }
   }
+}
+
+parser.codegen = function(context){
+  console.log(context)
+  if(context.type === "inst"){
+    context.fcn.apply(context,context.args);
+    return;
+  }
+  var statements = [];
+  for(var i = 0; i < context.length; i++){
+    statements[i] = parser.codegen.bind(parser,context[i]);
+  }
+  if(context.type === "switch"){
+    var breaks = [], cases = [], triggers = [];
+    for(var i = 0; i < context.length; i++){
+      if(context[i].type === "case"){
+        breaks.unshift(!context[i].break);
+        triggers.unshift(context[i].case);
+        cases.unshift(statements[i]);
+      }
+    }
+    fcns.switch(cases,triggers,function(){},breaks);
+    return;
+  }
+  var doAll = function(){
+    for(var i = 0; i < statements.length; i++){
+      statements[i]();
+    }
+  };
+  console.log(statements)
+  if(context.type === "whilenot"){
+    console.log("starting whilenot")
+    fcns.whilenot(0,doAll);
+    console.log("ending whilenot");
+    return;
+  }
+  doAll();
 }
--- testsuite.js
+++ testsuite.js
@@ -92,3 +92,38 @@ fcns.whilenot(0,function(){
 });
 },["",               ">",                  "[h-e>l+l<o] "],
   ["var s = [],p=0;","var s = [],p=0;p++;","var s = [],p=0;while(s[p]){s[p]=(s[p]||0)-1;p++;s[p]=(s[p]||0)+1;p--;}"]);
+
+
+test(function(){
+context = [[]];
+parser.parse("prints var s = [],p=0;\n"+
+"scans\n"+
+"whilenot\n"+
+  "switch\n"+
+    "case 93\n"+
+      "prints }\n"+
+      "break\n"+
+    "case 91\n"+
+      "prints while(s[p]){\n"+
+      "break\n"+
+    "case 62\n"+
+      "prints p++;\n"+
+      "break\n"+
+    "case 60\n"+
+      "prints p--;\n"+
+      "break\n"+
+    "case 46\n"+
+      "prints String.fromCharCode(s[p]||0);\n"+
+      "break\n"+
+    "case 45\n"+
+      "prints s[p]=(s[p]||0)-1;\n"+
+      "break\n"+
+    "case 43\n"+
+      "prints s[p]=(s[p]||0)+1;\n"+
+      "break\n"+
+  "end\n"+
+  "scans\n"+
+"end");
+parser.codegen(context);
+},["",               ">",                  "[h-e>l+l<o] "],
+  ["var s = [],p=0;","var s = [],p=0;p++;","var s = [],p=0;while(s[p]){s[p]=(s[p]||0)-1;p++;s[p]=(s[p]||0)+1;p--;}"]);
