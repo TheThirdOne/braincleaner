09e8f9654d519e5be2504931b91ab8cd05e71c67
Benjamin Landers <benjaminrlanders@gmail.com>
2015-11-28 20:06:41 -0800

    added inline tokenizer

--- tokenizer.js
+++ tokenizer.js
@@ -84,6 +84,47 @@ function getToken(){
   return 'Unexpected character: ' + lastChar;
 }
 
+function getInlineToken(){
+  var lastChar = input.getChar();
+  
+  while(!!lastChar.match(/\s/)){ //trim whitespace
+    lastChar = input.getChar();
+  }
+  if(lastChar === '/'){ //comments
+    lastChar = input.getChar();
+    if(lastChar === '/'){
+      while(lastChar != '\n' && lastChar != ''){
+        lastChar = input.getChar();
+      }
+    }else if(lastChar === '*'){
+      do{
+        lastChar = input.getChar();
+        while(lastChar != '*' && lastChar != ''){
+          lastChar = input.getChar();
+        }
+      }while(lastChar != '/' && lastChar != '');
+    }else{
+      throw "Unexpected / in inline function";
+    }
+    input.returnChar(lastChar);
+    return getToken();
+  }
+  if(lastChar === ''){ //pretty obvious stuff here
+    return {token:'EOF'};
+  }
+  if(!!lastChar.match(/[A-Za-z]/)){ //identifiers
+    return {token: 'identifier',
+            data:   getRest(/[A-Za-z0-9]/,lastChar),
+            index:  input.getIndex()};
+  }
+  if(!!lastChar.match(/[><\+\-\.,\[\]]/)){ //inline code
+    return {token: 'brainfuck',
+            data:   getRest(/[><\+\-\.,\[\]]/,lastChar),
+            index:input.getIndex()};
+  }
+  return 'Unexpected character in inline code: ' + lastChar;
+}
+
 function getRest(condition,last){ //helpful to grap a bunch of characters matching a regex
   var out = last || input.getChar();
   var lastChar = input.getChar();
