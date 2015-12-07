546b62a471f7328b3ecd3bfd26d665bb16271506
Benjamin Landers <benjaminrlanders@gmail.com>
2015-12-06 20:58:25 -0800

    bugfixes for first pass

--- compile.js
+++ compile.js
@@ -1,4 +1,5 @@
 function passOne(fcns, state){
+  //Moves functions to map, counts max blocks, args to map
   for(var i = 0; i < fcns.length; i++){
     state.fcns[fcns[i].name] = fcns[i];
     state.fcns[fcns[i].name].ifs = countifs(fcns[i].body,0);
@@ -6,7 +7,9 @@ function passOne(fcns, state){
     for(var k = 0; k < fcns[i].args.length;k++){
       fcns[i].arg[fcns[i].args[k]]=k;
     }
+    fcns[i].calls = {};
   }
+  //Checks all expressions (if, while, switch and assignment) and builds notes function calls
   for(var i = 0; i < fcns.length; i++){
     checkExprs(fcns[i].name,fcns[i].body,state);
   }
@@ -32,7 +35,7 @@ function countifs(block,current){
 function max(a,b){
   return (a>b)?a:b;
 }
-function checkExprs(name, body, state){
+function checkExprs(name, block, state){
   for(var i = 0; i < block.length;i++){
     if(block[i].type === 'while'){
       checkExprs(name,block[i].body,state);
@@ -47,18 +50,20 @@ function checkExprs(name, body, state){
     }
     if(block[i].type === 'statement'){ // make sure a main b += ... doesn't happen
       for(var k = 0; k < block[i].assignment.length; k++){
-        if(block[i].assignment[k].type !== 'identifier' || state.fcns[name].arg[block[i].assignment[k].data] === undefined){
+        if(block[i].assignment[k].token !== 'identifier' || state.fcns[name].arg[block[i].assignment[k].data] === undefined){
           throw "In Function " + name + ": Only variables allowed in assignment \"" + block[i].assignment.join(" ") + "\"";
         }
       }
     }
-    if(block[i].type === 'statement' && !!state.fcns[block[i].expression[0]]){ //for function calls
-      var fcn = state.fcns[block[i].expression[0]];
+    if(block[i].type === 'statement' && !!state.fcns[block[i].expression[0].data]){ //for function calls
+      var fcn = state.fcns[block[i].expression[0].data];
       state.fcns[name].calls[fcn.name] = true; //inform the parent function that this function is called
       block[i].calls = fcn.name;               //for later convience
       
-      for(var k = 0; k < block[i].expression.length; k++){ //check for ... main a b + h;
-        if(block[i].expression[k].type !== 'identifier' || state.fcns[name].arg[block[i].expression[k].data] === undefined){
+      for(var k = 1; k < block[i].expression.length; k++){ //check for ... main a b + h;
+        
+          console.log(block[i].expression[k])
+        if(block[i].expression[k].token !== 'identifier' || state.fcns[name].arg[block[i].expression[k].data] === undefined){
           throw "In Function " + name + ": Only variables allowed in function calls \"" + block[i].expression.join(" ") + "\"";
         }
       }
@@ -68,15 +73,16 @@ function checkExprs(name, body, state){
     }else{
       var stack = 0;
       for(var k = 0; k < block[i].expression.length; k++){ //check for ... main a b + h;
-        if(block[i].expression[k].type !== 'identifier' || state.fcns[name].arg[block[i].expression[k].data] === undefined){
+        if((block[i].expression[k].token === 'identifier' && state.fcns[name].arg[block[i].expression[k].data] !== undefined)
+           || block[i].expression[k].token === 'number'){
           stack++;
-        }else if(block[i].expression[k].type !== '+' || block[i].expression[k].type !== '-' || block[i].expression[k].type !== '*' || block[i].expression[k].type !== '%'){
+        }else{
           stack--;
         }
       }
       if(stack !== 1){
         throw "In Function " + name + ": Expression must result in a stack of size 1. Resulted in stack of size " + stack + ". \"" + block[i].expression.join(" ") + "\"";
-      }d
+      }
     }
   }
 }
